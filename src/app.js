c4ff6d158f7c7cb4706530462c940c5098907c8d
Saiful Islam <nysaifulislam@gmail.com>
2022-01-20 18:55:47 -0500

    setup prettier formatting

--- src/app.js
+++ src/app.js
@@ -24,13 +24,13 @@ app.use(morgan(logFormat));
 app.use('/api', require('./routes'));
 
 app.get('/', (req, res) => {
-    res.json({ msg: 'Welcome to BudCode!' });
+  res.json({ msg: 'Welcome to BudCode!' });
 });
 
 // start up server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
 
 // connect to DB
--- src/utils/connectToDB.js
+++ src/utils/connectToDB.js
@@ -7,17 +7,17 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const { DB_DATABASE, DB_USER, DB_PASS, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASS | null, {
-    host: DB_HOST,
-    dialect: 'postgres',
+  host: DB_HOST,
+  dialect: 'postgres',
 });
 
 const connectToDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Database connected');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 };
 
 module.exports = connectToDB;
