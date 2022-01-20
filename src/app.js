c4e2864b762417dd9fb702913a93299886e32ee6
Saiful Islam <nysaifulislam@gmail.com>
2022-01-19 20:05:05 -0500

    audit app use using morgan

--- src/app.js
+++ src/app.js
@@ -1,32 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const morgan = require('morgan');
+
+// use environment variables
+dotenv.config();
 
 // intiialize app
-dotenv.config();
 const app = express();
 
 // this lets us parse 'application/json' content in http requests
-app.use(express.json()); 
+app.use(express.json());
 
+// make server accessible to any domain
 app.use(cors());
 
-// add http request logging to help us debug and audit app use
-// const logFormat = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
-// app.use(morgan(logFormat));
+// add http request logging to debug and audit app use
+const logFormat = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+app.use(morgan(logFormat));
 
-// this mounts controllers/index.js at the route `/api`
+// this mounts routes/index.js at the route `/api`
 app.use('/api', require('./routes'));
-
-// connect to DB
 
 app.get('/', (req, res) => {
     res.json({ msg: 'Welcome to BudCode!' });
 });
-
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+
+// connect to DB
