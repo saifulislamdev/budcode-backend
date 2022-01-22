68041a527361644dad57c65eb5e290df784433b5
Saiful Islam <nysaifulislam@gmail.com>
2022-01-21 19:55:08 -0500

    connect to DB using pool

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,18 +1,28 @@
+const { Pool } = require('pg');
 const path = require('path');
 
 // import environment variables
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
-const { DB_DATABASE, DB_USER, DB_PASS, DB_HOST } = process.env;
+const { DB_USER, DB_HOST, DB_DATABASE, DB_PASS, DB_PORT } = process.env;
 
-// const connectToDB = async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Database connected');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-//   sequelize.sync({ alter: true });
-// };
+const pool = new Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  password: DB_PASS,
+  port: DB_PORT,
+});
 
-// module.exports = { connectToDB };
+const verifyDBConnection = async () => {
+  try {
+    // connect with a connection pool
+    const poolResult = await pool.query('SELECT NOW()');
+    console.log('Database connected');
+    console.log('Time connected: ' + poolResult.rows[0]['now']);
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+module.exports = { pool, verifyDBConnection };
