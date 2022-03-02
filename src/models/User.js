234e3d9eb71d0a94d2c8c6ad7bfbddf31f642ba6
abdulimtiaz <abdulimtiaz98@gmail.com>
2022-03-02 02:36:14 -0500

    changed username and email types to CITEXT

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,10 @@
 const User =
     'CREATE TABLE IF NOT EXISTS "User" (\
-        username VARCHAR(255), \
+        username CITEXT, \
         password VARCHAR(255) NOT NULL, \
         first_name VARCHAR(255) NOT NULL, \
         last_name VARCHAR(255), \
-        email VARCHAR(255) UNIQUE, \
+        email CITEXT UNIQUE, \
         bio VARCHAR(255), \
         occupation VARCHAR(255), \
         gender VARCHAR(255), \
