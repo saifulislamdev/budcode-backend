3ed8292092a82138eda723bd0a9ae41d8d86d924
Saiful Islam <nysaifulislam@gmail.com>
2022-04-26 17:48:47 -0400

    added inner-try block to allow for inserting of other records upon failure of one record

--- src/utils/mock_data/createUsersMockData.js
+++ src/utils/mock_data/createUsersMockData.js
@@ -12,54 +12,57 @@ const createUsersMockData = async () => {
         for (const userInfo of usersInfos) {
             const hashedPassword = await bcrypt.hash(userInfo.password, salt);
 
-            await pool.query(
-                '\
+            // Inner try-catch to make sure if one statement fails, the subsequent ones don't
+            try {
+                await pool.query(
+                    '\
                     INSERT INTO "User"\
                         (username, password, first_name, last_name, email, bio, occupation, gender)\
                     VALUES\
                         ($1, $2, $3, $4, $5, $6, $7, $8)\
                     ',
-                [
-                    userInfo.username,
-                    hashedPassword,
-                    userInfo.first_name,
-                    userInfo.last_name,
-                    userInfo.email,
-                    userInfo.bio,
-                    userInfo.occupation,
-                    userInfo.gender,
-                ]
-            );
+                    [
+                        userInfo.username,
+                        hashedPassword,
+                        userInfo.first_name,
+                        userInfo.last_name,
+                        userInfo.email,
+                        userInfo.bio,
+                        userInfo.occupation,
+                        userInfo.gender,
+                    ]
+                );
 
-            if (userInfo.links)
-                for (const link of userInfo.links) {
-                    promises.push(
-                        pool.query(
-                            'INSERT INTO "UserLink"(username, link, type) VALUES ($1, $2, $3)',
-                            [userInfo.username, link.link, link.type]
-                        )
-                    );
-                }
+                if (userInfo.links)
+                    for (const link of userInfo.links) {
+                        promises.push(
+                            pool.query(
+                                'INSERT INTO "UserLink"(username, link, type) VALUES ($1, $2, $3)',
+                                [userInfo.username, link.link, link.type]
+                            )
+                        );
+                    }
 
-            if (userInfo.interests)
-                for (const interest of userInfo.interests) {
-                    promises.push(
-                        pool.query(
-                            'INSERT INTO "UserInterest"(username, interest) VALUES($1, $2)',
-                            [userInfo.username, interest]
-                        )
-                    );
-                }
+                if (userInfo.interests)
+                    for (const interest of userInfo.interests) {
+                        promises.push(
+                            pool.query(
+                                'INSERT INTO "UserInterest"(username, interest) VALUES($1, $2)',
+                                [userInfo.username, interest]
+                            )
+                        );
+                    }
 
-            if (userInfo.skills)
-                for (const skill of userInfo.skills) {
-                    promises.push(
-                        pool.query(
-                            'INSERT INTO "UserSkill"(username, skill) VALUES($1, $2)',
-                            [userInfo.username, skill]
-                        )
-                    );
-                }
+                if (userInfo.skills)
+                    for (const skill of userInfo.skills) {
+                        promises.push(
+                            pool.query(
+                                'INSERT INTO "UserSkill"(username, skill) VALUES($1, $2)',
+                                [userInfo.username, skill]
+                            )
+                        );
+                    }
+            } catch (err) {}
         }
 
         await Promise.all(promises);
