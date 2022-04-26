04f3b99ce5a11dda5966140ae82700872f7e2cee
Saiful Islam <nysaifulislam@gmail.com>
2022-04-26 17:57:00 -0400

    added projects followers mock data

---
+++ src/utils/mock_data/createProjectFollowers.js
@@ -0,0 +1,36 @@
+const { pool } = require('../db');
+const projectsFollowers = require('./projectsFollowers');
+
+// Creates mock project followers data
+const createProjectsFollowersMockData = async () => {
+    try {
+        for (const projectFollower of projectsFollowers) {
+            // Inner try-catch to make sure if one statement fails, the subsequent ones don't
+            try {
+                // Insert new record (if not added already)
+                await pool.query(
+                    '\
+                INSERT INTO "ProjectFollower"\
+                    (project_id, username)\
+                VALUES\
+                    ($1, $2)\
+                ',
+                    [projectFollower.project_id, projectFollower.username]
+                );
+
+                // Update following status to true
+                // (in case it was previously false from previous record)
+                await pool.query(
+                    '\
+                UPDATE "ProjectFollower" \
+                SET status = true \
+                WHERE project_id = $1 AND username = $2\
+                ',
+                    [projectFollower.project_id, projectFollower.username]
+                );
+            } catch (err) {}
+        }
+    } catch (err) {}
+};
+
+module.exports = createProjectsFollowersMockData;
--- src/utils/seedDB.js
+++ src/utils/seedDB.js
@@ -1,8 +1,10 @@
 const createUsers = require('./mock_data/createUsers');
+const createProjectFollowers = require('./mock_data/createProjectFollowers');
 
 // Seeding DB with mock data
 const seed = async () => {
     await createUsers();
+    await createProjectFollowers();
 };
 
 module.exports = seed;
