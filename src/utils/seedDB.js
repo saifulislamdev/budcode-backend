812215af90e8b17910d97b2a30ffbf5e9150004d
abdulimtiaz <abdulimtiaz98@gmail.com>
2022-05-03 22:12:01 -0400

    added .env variable check for inserting mock data

--- src/utils/seedDB.js
+++ src/utils/seedDB.js
@@ -1,9 +1,21 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+const createJoinRequests = require('./mock_data/createJoinRequests');
 const createProjects = require('./mock_data/createProjects');
 const createUsersMockData = require('./mock_data/createUsers');
+const handleJoinRequests = require('./mock_data/handleJoinRequests');
+
+
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const seed = async () => {
-    await createUsersMockData();
-    await createProjects();
+    if(process.env.MOCK_DATA_INSERTION === 'true') {
+        await createUsersMockData();
+        await createProjects();
+        await createJoinRequests();
+        await handleJoinRequests();
+    }
 };
 
 module.exports = seed;
