58c81ad8dffe8528976a405144e2b0574ed71a72
Saiful Islam <nysaifulislam@gmail.com>
2022-05-08 01:18:51 -0400

    replaced hard-coded initial value for id + prettier formatting in file

--- src/utils/mock_data/pullData.js
+++ src/utils/mock_data/pullData.js
@@ -2,6 +2,7 @@ const { Octokit } = require('@octokit/core');
 const path = require('path');
 const dotenv = require('dotenv');
 const usersInfos = require('./users');
+const initialProjects = require('./initialProjects');
 
 // range: [min, max)
 function getRandomInt(min, max) {
@@ -9,7 +10,6 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
-  
 
 dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 
@@ -20,19 +20,19 @@ function repoFormat(repo, id, creator) {
     project.creator = creator;
     project.description = repo.description;
     project.skills = repo.languages;
-    project.tags = repo.topics.map(topic => topic.replace(new RegExp('-', 'g'),' '));
-    project.links = [{ type: 'GitHub', link: repo.html_url }]
+    project.tags = repo.topics.map((topic) =>
+        topic.replace(new RegExp('-', 'g'), ' ')
+    );
+    project.links = [{ type: 'GitHub', link: repo.html_url }];
 
     return project;
 }
-
 
 /**
  * @param {string} query - search query
  */
 const fetchRepos = async (query) => {
-
-    const octokit = new Octokit({auth: process.env.GITAUTH});
+    const octokit = new Octokit({ auth: process.env.GITAUTH });
     let params = new URLSearchParams();
     params.append('q', query);
     params.append('sort', 'stars');
@@ -40,24 +40,28 @@ const fetchRepos = async (query) => {
     params.append('per_page', '100');
     params.append('page', '1');
     let url = `/search/repositories?${params.toString()}`;
-    
+
     const searchResult = await octokit.request(`GET ${url}`, {});
     let searchData = searchResult.data.items;
 
     // get rid of repos with no tags or description
-    searchData = searchData.filter(repo => repo.topics.length > 0 && repo.description != null);
+    searchData = searchData.filter(
+        (repo) => repo.topics.length > 0 && repo.description != null
+    );
 
     // get languages associated with project
 
-    await Promise.all(searchData.map(async (repo) => {
-        url = `/repos/${repo.owner.login}/${repo.name}/languages`;
-        let lang = await octokit.request(`GET ${url}`, {});
-        repo.languages = Object.keys(lang.data);
-    }));
+    await Promise.all(
+        searchData.map(async (repo) => {
+            url = `/repos/${repo.owner.login}/${repo.name}/languages`;
+            let lang = await octokit.request(`GET ${url}`, {});
+            repo.languages = Object.keys(lang.data);
+        })
+    );
 
     // get rid of repos with no associated languages
-    searchData = searchData.filter(repo => repo.languages.length > 0);
-    
+    searchData = searchData.filter((repo) => repo.languages.length > 0);
+
     return searchData;
 };
 
@@ -66,9 +70,9 @@ const fetchRepos = async (query) => {
  * @return {Promise<Array<Object>>} returns a promise that is fulfilled with an array of objects representing projects
  */
 const generateProjects = async (topics) => {
-
-    // start at id=6 because of reserved first 5 ids
-    let id = 11;
+    // start at id > 1 because of reserved ids in ./initialProjects.js
+    const lastInitialProjectId = initialProjects.at(-1).id;
+    let id = lastInitialProjectId + 1;
 
     // up to 100 projects will be fetched for each topic
     // the actual number may be smaller due to filtering out projects without tags/skills/description
@@ -85,7 +89,9 @@ const generateProjects = async (topics) => {
     for (let i = 0; i < searches; ++i) {
         const repos = await fetchRepos(topics[i]);
         repos.forEach((repo) => {
-            process.stdout.write(`\rAdding project ${id} from page ${i+1}...`);
+            process.stdout.write(
+                `\rAdding project ${id} from page ${i + 1}...`
+            );
             let creator = usersInfos[getRandomInt(0, 20)].username;
             projects.push(repoFormat(repo, id, creator));
             ++id;
