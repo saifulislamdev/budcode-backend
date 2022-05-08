2fca7546ee141849c50c066b94933b54d973f95e
Saiful Islam <nysaifulislam@gmail.com>
2022-05-08 00:02:24 -0400

    rearranged order of mock data creations to insert referenced rows first

--- src/utils/seedDB.js
+++ src/utils/seedDB.js
@@ -6,7 +6,6 @@ const createProjects = require('./mock_data/createProjects');
 const createProjectFollowers = require('./mock_data/createProjectFollowers');
 const createProjectUpdates = require('./mock_data/createProjectUpdates');
 const createUsers = require('./mock_data/createUsers');
-const createUsersMockData = require('./mock_data/createUsers');
 const createUserReviews = require('./mock_data/createUserReviews');
 const handleJoinRequests = require('./mock_data/handleJoinRequests');
 
@@ -15,15 +14,38 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 // Seeding DB with mock data
 const seed = async () => {
     if (process.env.MOCK_DATA_INSERTION === 'true') {
+        // Creating users
         await createUsers();
-        await createUsersMockData();
+
+        // Creating projects
+        // Relies on successful creation of users
         await createProjects();
+
+        // Creating project followers
+        // Relies on successful creation of users and projects
         await createProjectFollowers();
-        await createProjectUpdates();
+
+        // Creating project members for projects
+        // Relies on successful creation of users and projects
         await createJoinRequests();
-        await createUserReviews();
+        // Relies on successful creation of join requests
         await handleJoinRequests();
-    }
+
+        // Creation of project updates
+        // Relies on successful handling of join requests
+        await createProjectUpdates();
+
+        // Creating user reviews
+        // Relies on successful handling of join requests
+        await createUserReviews();
+
+        console.log(
+            'Mock data: Mock data creation finished or enough data in tables already'
+        );
+    } else
+        console.log(
+            'To use mock data, set MOCK_DATA_INSERTION environment variable'
+        );
 };
 
 module.exports = seed;
