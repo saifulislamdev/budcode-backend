a2c7403920a84ff0b8dd007252d3f33c2e491894
Saiful Islam <nysaifulislam@gmail.com>
2022-05-12 11:08:54 -0400

    fixed ambigious column error in checkReviewability

--- src/helpers/checkReviewability.js
+++ src/helpers/checkReviewability.js
@@ -6,31 +6,29 @@
 const { pool } = require('../utils/db');
 
 const checkReviewability = async (reviewer, reviewed) => {
-
     // check if users share any completed projects
-    const {rowCount : mutuallyCompleted} = await pool.query(
+    const { rowCount: mutuallyCompleted } = await pool.query(
         `SELECT project_id 
             FROM "ProjectMember" 
             JOIN "Project"
             ON project_id = id
             WHERE username IN ($1, $2)
-            AND status = 'Complete' 
+            AND "Project".status = 'Complete' 
             GROUP BY project_id 
             HAVING COUNT(DISTINCT username) > 1`,
-            [reviewer, reviewed]
+        [reviewer, reviewed]
     );
 
     if (mutuallyCompleted) {
-
         // check if user has already reviewed the target user
-        const {rowCount : reviewExists} = await pool.query(
+        const { rowCount: reviewExists } = await pool.query(
             `SELECT 1 
                 FROM "UserReview" 
                 WHERE reviewer_username = $1
                 AND reviewed_username = $2`,
-                [reviewer, reviewed]
+            [reviewer, reviewed]
         );
-        
+
         if (!reviewExists) {
             return true;
         }
